9db5e4dc75a99a9c7d8d4ed1b90b37e4d716563b
Olga Iwanova <iwanowa.dd2016@yandex.ru>
2018-06-27 14:31:57 +0300

    one

---
+++ dir.cpp
@@ -0,0 +1,19 @@
+
+int i;
+switch(i) {
+    
+    for (int j=0; j<10; j++) {
+        
+        //todo 1
+        
+        case 1: 
+            
+            //todo 2
+            
+        break;
+        
+        //todo 3
+        
+    }
+    
+}
